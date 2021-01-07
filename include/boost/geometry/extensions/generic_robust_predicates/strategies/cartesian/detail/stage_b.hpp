8cfabe769993a7e6384136a5db39a667b7dfb51b
Tinko Bartels <mail@tinkobartels.de>
2021-01-07 11:14:43 +0100

    do tailcheck first, then calculate stage b

--- include/boost/geometry/extensions/generic_robust_predicates/strategies/cartesian/detail/stage_b.hpp
+++ include/boost/geometry/extensions/generic_robust_predicates/strategies/cartesian/detail/stage_b.hpp
@@ -73,10 +73,10 @@ struct all_differences_zero_tail
             {{ static_cast<Real>(args)... }};
         Real left_val = input[left::argn - 1];
         Real right_val = input[right::argn - 1];
-        *(begin + start) = left_val - right_val;
         using eval_index = boost::mp11::mp_find<Evals, eval>;
         constexpr std::size_t start =
             boost::mp11::mp_at<AccumulatedSizes, eval_index>::value;
+        *(begin + start) = left_val - right_val;
         if(two_difference_tail(left_val, right_val, *(begin + start))
                 == Real(0))
         {
@@ -168,7 +168,14 @@ struct stage_b
             return sign_uncertain;
         }
 
-        using remainder = typename boost::mp11::mp_remove_if<eval, is_leaf_difference>;
+        using remainder = typename boost::mp11::mp_remove_if<evals, is_leaf_difference>;
+
+        using ze_evals = boost::mp11::mp_copy_if_q
+        <
+            boost::mp11::mp_pop_back<remainder>,
+            is_zero_elim_q<ZEPolicy, true>
+        >;
+        std::array<typename result_array::iterator, boost::mp11::mp_size<ze_evals>::value> ze_ends;
 
         auto most_significant = eval_expansions_impl
             <
@@ -176,12 +183,13 @@ struct stage_b
                 remainder,
                 sizes,
                 accumulated_sizes,
+                ze_evals,
                 decltype(results.begin()),
                 Real,
-                true,
                 ZEPolicy,
-                FEPolicy
-            >::apply(results.begin(), results.end(), args...) - 1;
+                FEPolicy,
+                true
+            >::apply(results.begin(), results.end(), ze_ends, args...) - 1;
 
         if( *most_significant == 0)
         {
